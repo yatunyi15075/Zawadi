ad458f29b78a3b97c54e2f2b006a0c29435ec21d
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-05-26 05:54:05 -0700

    Update ParentDashboard.jsx

--- frontend/src/pages/Parents/ParentDashboard.jsx
+++ frontend/src/pages/Parents/ParentDashboard.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import ChildProgress from './ChildProgress';
+import ChildGrades from './Grades';
+import Attendance from './Attendance';
+import Communication from './Communication';
+
+// Styled components
+const DashboardContainer = styled.div`
+  padding: 20px;
+`;
+
+const SectionContainer = styled.div`
+  margin-bottom: 30px;
+`;
+
+const SectionTitle = styled.h2`
+  margin-bottom: 10px;
+`;
 
 const ParentDashboard = () => {
   return (
-    <div>ParentDashboard</div>
-  )
+    <DashboardContainer>
+      <SectionContainer>
+        <SectionTitle>Child Progress</SectionTitle>
+        <ChildProgress />
+      </SectionContainer>
+
+      <SectionContainer>
+        <SectionTitle>Grades</SectionTitle>
+        <ChildGrades />
+      </SectionContainer>
+
+      <SectionContainer>
+        <SectionTitle>Attendance</SectionTitle>
+        <Attendance />
+      </SectionContainer>
+
+      <SectionContainer>
+        <SectionTitle>Communication</SectionTitle>
+        <Communication />
+      </SectionContainer>
+    </DashboardContainer>
+  );
 }
 
-export default ParentDashboard
+export default ParentDashboard;
