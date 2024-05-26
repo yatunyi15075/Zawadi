8572053375007df8aad8f2a3bdbf8daa85fa7f4e
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-05-26 02:17:57 -0700

    Update Reports.jsx

--- frontend/src/pages/Admin/Reports.jsx
+++ frontend/src/pages/Admin/Reports.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import AttendanceReport from './AttendanceReport';
+import StudentPerformanceReport from './StudentPerformanceReport';
+import AssessmentReport from './AssessmentReport';
+import ProgressReport from './ProgressReport';
+import Sidebar from './Sidebar';
 
 const Reports = () => {
-  return (
-    <div>Reports</div>
-  )
+    return (
+        <div>
+          <Sidebar />
+            <h2>Individual Reports</h2>
+            <AttendanceReport />
+            <StudentPerformanceReport />
+            <AssessmentReport />
+            <ProgressReport />
+        </div>
+    );
 }
 
 export default Reports
