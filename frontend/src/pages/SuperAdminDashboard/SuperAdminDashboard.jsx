b0a435f45442cc0f7435e1ff8e9422db0c2ec49d
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-05-26 05:58:51 -0700

    Update SuperAdminDashboard.jsx

--- frontend/src/pages/SuperAdminDashboard/SuperAdminDashboard.jsx
+++ frontend/src/pages/SuperAdminDashboard/SuperAdminDashboard.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import AdvancedAnalytics from './AdvancedAnalytics';
+import IntegrationOptions from './IntergrationOptions';
+import SystemSettings from './SystemSettings';
+import UserManagement from './UserManagement';
+
+// Styled components
+const DashboardContainer = styled.div`
+  padding: 20px;
+`;
+
+const SectionHeading = styled.h2`
+  margin-bottom: 20px;
+`;
 
 const SuperAdminDashboard = () => {
   return (
-    <div>SuperAdminDashboard</div>
-  )
-}
+    <DashboardContainer>
+      <SectionHeading>Advanced Analytics</SectionHeading>
+      <AdvancedAnalytics />
 
-export default SuperAdminDashboard
+      <SectionHeading>Integration Options</SectionHeading>
+      <IntegrationOptions />
+
+      <SectionHeading>System Settings</SectionHeading>
+      <SystemSettings />
+
+      <SectionHeading>User Management</SectionHeading>
+      <UserManagement />
+    </DashboardContainer>
+  );
+};
+
+export default SuperAdminDashboard;
